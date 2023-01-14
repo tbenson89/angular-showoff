f0a7ddcab4df171a6e2f33f092bbc433c137f00a
tbenson89 <typrobenson@gmail.com>
2023-01-14 10:41:30 -0700

    added tailwind and updated home
    Still working around on the home and how I want to layout the different subjects and how to show off concepts

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.scss']
+  styleUrls: ['./top-nav.component.scss'],
 })
 export class TopNavComponent implements OnInit {
+  isMenuOpen = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
-
 }
